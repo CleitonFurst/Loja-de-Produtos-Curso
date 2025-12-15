2c712a8d37d189fac30016dc8615207ec87cd2cf
CleitonFurst <furstcleiton@gmail.com>
2025-12-15 09:58:17 -0300

    Usuarios

---
+++ LojaProdutosCurso/Models/UsuarioModel.cs
@@ -0,0 +1,22 @@
+﻿using LojaProdutosCurso.Enums;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.VisualBasic;
+
+namespace LojaProdutosCurso.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public byte[] SenhaHash { get; set; }
+        public byte[] SenhaSalt { get; set; }
+        public CargoEnum Cargo { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public int EnderecoModelId { get; set; }
+
+        [ValidateNever]
+        public EnderecoModel Endereco { get; set; }
+    }
+}
